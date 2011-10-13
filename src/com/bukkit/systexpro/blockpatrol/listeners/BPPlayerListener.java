e456164124cd7eb4dffdd76a166fa0369932f02d
Christian Horton <christian@tweakuniverse.com>
2011-10-12 22:20:13 -0700

    Block Listener

--- src/com/bukkit/systexpro/blockpatrol/listeners/BPPlayerListener.java
+++ src/com/bukkit/systexpro/blockpatrol/listeners/BPPlayerListener.java
@@ -1,5 +1,9 @@
 package com.bukkit.systexpro.blockpatrol.listeners;
 
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerListener;
 import org.bukkit.event.player.PlayerInteractEvent;
 import com.bukkit.systexpro.blockpatrol.BlockPatrolPlugin;
@@ -13,6 +17,10 @@ public class BPPlayerListener extends PlayerListener {
 	}
 	
 	public void onPlayerInteractEvent(PlayerInteractEvent event) {
-		
+		Player p = event.getPlayer();
+		Block b = event.getClickedBlock();
+		if(event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+			plugin.getEventHandler().getBlock(b, p);
+		}
 	}
 }
