d4bd64a464f83144d5cea233fcba7fff7f449870
Christian Horton <christian@tweakuniverse.com>
2011-10-13 19:22:36 -0700

    Added to check for SQL/MySQL. Added to create Databases on startup.

--- src/com/bukkit/systexpro/blockpatrol/BlockPatrolPlugin.java
+++ src/com/bukkit/systexpro/blockpatrol/BlockPatrolPlugin.java
@@ -1,5 +1,9 @@
 package com.bukkit.systexpro.blockpatrol;
 
+import org.bukkit.event.Event.Priority;
+import org.bukkit.event.Event.Type;
+import org.bukkit.event.block.BlockListener;
+import org.bukkit.event.player.PlayerListener;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -7,18 +11,24 @@ import com.bukkit.systexpro.blockpatrol.commands.CommandHandler;
 import com.bukkit.systexpro.blockpatrol.commands.HelpCommand;
 import com.bukkit.systexpro.blockpatrol.commands.RollbackCommand;
 import com.bukkit.systexpro.blockpatrol.commands.WandCommand;
+import com.bukkit.systexpro.blockpatrol.events.EventHandler;
+import com.bukkit.systexpro.blockpatrol.galert.BPGriefAlert;
+import com.bukkit.systexpro.blockpatrol.listeners.BPBlockListener;
+import com.bukkit.systexpro.blockpatrol.listeners.BPPlayerListener;
 import com.bukkit.systexpro.blockpatrol.settings.BlockPatrolSettings;
 
 public class BlockPatrolPlugin extends JavaPlugin {
 
 	public BlockPatrolSettings settings;
+	public PlayerListener pListener = new BPPlayerListener(this);
+	public BlockListener bListener = new BPBlockListener(this);
+	public BPGriefAlert griefAlert = new BPGriefAlert(this);
+	public EventHandler eventHandler = new EventHandler(this);
 	
-	@Override
 	public void onDisable() {
-		sendConsoleMessage("Disabled");
-		
+		sendConsoleMessage("Disabled");	
 	}
-
+	
 	@Override
 	public void onEnable() {
 		//Load Settings/Make Folder
@@ -26,26 +36,45 @@ public class BlockPatrolPlugin extends JavaPlugin {
 		settings = new BlockPatrolSettings(this);
 		settings.loadConfig();
 		
+		//Start SQL
+//		settings.setupFileSaver();
+		settings.createSQLDatabase();
+		
+		//Send enable message
 		sendConsoleMessage("Enabled Version: " + this.getDescription().getVersion() + " by " + settings.getAuthors());
 		
 		//Register Events
 		registerEvents();
 		
+		//Start GriefAlert
+		griefAlert.init();
+		
 		//Register Commands
-		CommandHandler cmd = new CommandHandler();
+		CommandHandler cmd = new CommandHandler(this);
 		cmd.registerExecutor("rollback", new RollbackCommand());
+		cmd.registerExecutor("wand", new WandCommand(this));
 		cmd.registerExecutor("help", new HelpCommand());
 		cmd.registerExecutor("?", new HelpCommand());
-		cmd.registerExecutor("wand", new WandCommand(this));
 		getCommand("bp").setExecutor(cmd);
 	}
 	
+	/**
+	 * Register the Events
+	 */
 	private void registerEvents() {
 		PluginManager pm = getServer().getPluginManager();
-//		pm.registerEvent(Event.Type.PLAYER_INTERACT, playerListener, Priority.Normal, this);
-
+		pm.registerEvent(Type.PLAYER_INTERACT, pListener, Priority.Normal, this);
+		pm.registerEvent(Type.BLOCK_PLACE, bListener, Priority.Normal, this);
 	}
 	
+	public EventHandler getEventHandler() {
+		return eventHandler;
+	}
+	
+	/**
+	 * Send a Console Message
+	 * @param arg0
+	 */
 	public void sendConsoleMessage(String arg0) {
 		System.out.println("[BlockPatrol] " + arg0);
 	}
